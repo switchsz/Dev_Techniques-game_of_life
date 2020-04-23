650f2413d9b2ad4d150fdfdee008d673e9002dcb
Saida Sideifzade <saidasideifzada@gmail.com>
2020-04-23 14:39:01 +0400

    Cell Struct

---
+++ cell.h
@@ -0,0 +1,14 @@
+struct Cell {
+    int life;
+    int x;
+    int y;
+    Cell *upleft;
+    Cell *up;
+    Cell *upright;
+    Cell *left;
+    Cell *right;
+    Cell *downleft;
+    Cell *down;
+    Cell *downright;
+
+} typedef Cell;
